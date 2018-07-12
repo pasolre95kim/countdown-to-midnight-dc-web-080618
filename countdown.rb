89edb6eb673ed76a9c82fb9040f1113488ab6a0c
Michelle Kim <pasolre95kim@gmail.com>
2018-07-12 22:10:06 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,10 @@
 #write your code here
 
 def countdown(n)
-  n = 10
-  while n < 1
+  n = 1
+  while n < 10
     puts "#{n} SECOND(S)!"
-    n -= 1
-
+    n += 1
+      break if n == 1
    end
 end
